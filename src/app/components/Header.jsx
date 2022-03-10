8ae015df51d5feec6abb3d98252099cc023466a1
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-03-10 15:46:33 -0300

    feat: adicionando um componente Header a mainPage

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,17 @@
 // This component render a Header.
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="header">
+      <div id="velas">
+        <div id="vela1"></div>
+        <div id="vela2"></div>
+        <div id="vela3"></div>
+      </div>
+      <div id="header-hr"></div>
+      <div id="header-text"><p id="p1">Análise Geral</p><p id="p2">/</p><p id="p3">Principal</p></div>
     </div>
   );
 }
