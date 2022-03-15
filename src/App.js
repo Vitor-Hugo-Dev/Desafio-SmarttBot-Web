2f2077eb578f1c19c55805f2b41b4180bf52944e
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-03-15 17:44:35 -0300

    fix: testando correção de deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Mainpage from "./app/pages/MainPage";
+import Provider from "./context/provider";
+
+function App() {
+
+  return (
+  <Provider>
+    <div className="App">
+      <Mainpage />
+    </div>
+  </Provider>  
+  );
+}
+
+export default App;
