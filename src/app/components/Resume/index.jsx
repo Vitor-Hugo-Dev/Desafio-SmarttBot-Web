d540f3a7a39b47d8fab2f4fa2b85d98150196043
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-03-15 13:40:30 -0300

    refactor: agora as informações do componente Resume, vem da Api

--- src/app/components/Resume/index.jsx
+++ src/app/components/Resume/index.jsx
@@ -1,22 +1,39 @@
-// this component render a informations resume. Adn stay later of Header.
-import React from 'react';
-import Paperline from "../PaperLine";
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import Paperline from '../PaperLine';
 import './style.css';
+import robotRequests from '../../../services/robotServices';
 
 const Resume = () => {
+  const [overView, setOverView] = useState({
+    data: { papers: [], transactions: '', moviment_summary: 0 },
+  });
+
+  useEffect(() => {
+    getOverviews();
+  }, []);
+
+  const getOverviews = async () => {
+    const response = await robotRequests.getOverviews();
+    setOverView(response);
+  };
+  const { papers, transactions, moviment_summary } = overView.data;
+
   return (
     <div className="resume">
       <div className="resume-infos">
         <div id="info1">
           <p id="info1-title">Resumo geral operações</p>
-          <div>
+          <div id="info1-container">
             <p id="info1-subtitle">Resumo de movimentação</p>
-            <p id="info1-value">-R$220,00</p>
+            <p id="info1-value">{`${
+              moviment_summary < 0 ? '-' : ''
+            }R$${moviment_summary}`}</p>
           </div>
         </div>
         <div id="info2">
           <p id="info2-subtitle">Total de transações realizadas</p>
-          <p id="info2-value">443</p>
+          <p id="info2-value">{transactions}</p>
         </div>
       </div>
       <div id="resume-hr" />
@@ -24,16 +41,32 @@ const Resume = () => {
         <p id="papers-title">Papéis negociados</p>
         <div id="papers-papers">
           <div>
-          {[1, 2, 3].map(() => (<Paperline />))}
+            {papers.map(({ name, trasactions }) => (
+              <Paperline
+                key={name + trasactions}
+                paper={{ name, trasactions }}
+              />
+            ))}
           </div>
           <div>
-          {[1, 2, 3].map(() => (<Paperline />))}
+            {overView.data.papers.map(({ name, trasactions }) => (
+              <Paperline
+                key={name + trasactions}
+                paper={{ name, trasactions }}
+              />
+            ))}
           </div>
         </div>
-
       </div>
     </div>
   );
+};
+
+Resume.propTypes = {
+  overView: PropTypes.shape({
+    papers: PropTypes.array.isRequired,
+    transactions: PropTypes.string.isRequired,
+  }),
 };
 
 export default Resume;
