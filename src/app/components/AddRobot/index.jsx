197defe0ee022e63adcb0ffbf60ef686c3415717
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-03-15 13:39:19 -0300

    feat: adicionado a função do botão para exibir o modal

--- src/app/components/AddRobot/index.jsx
+++ src/app/components/AddRobot/index.jsx
@@ -1,16 +1,17 @@
 // this area from take a new robot
+import PropTypes from "prop-types"
 import React from 'react';
 import './style.css'
 import robotIcon from '../../../images/robotIcon.svg';
 
-const Addrobotinput = () => {
+const Addrobotinput = (props) => {
   return (
     <div id="add-robot">
       <div id="add-robot-icon">
         <img src={robotIcon} alt="icon" />
       </div>
       <div id="add-robot-container">
-        <div id="add-robot-button">Adicionar novo Robô</div>
+        <div id="add-robot-button" onClick={() => {props.createRobot(true)}}>Adicionar novo Robô</div>
         <div id="add-robot-text">
           <p id="add-robot-text1"> Você possui</p>
           <p id="add-robot-text2">02 Robôs</p>
@@ -19,6 +20,10 @@ const Addrobotinput = () => {
       </div>
     </div>
   );
+}
+
+Addrobotinput.propTypes = {
+  createRobot: PropTypes.func.isRequired
 }
 
 export default Addrobotinput;
